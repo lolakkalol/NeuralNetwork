1c3d6739695a7bd7bb674d5dc5106ea941387ac2
lolakkalol <alexander.stenlund@telia.com>
2020-07-05 17:53:44 +0200

    Added random weights in addConnection() same with bias

--- src/Neuron.cpp
+++ src/Neuron.cpp
@@ -3,16 +3,15 @@
 //
 // Written by Alexander Stenlund, Västerås, Sweden (c) 2020
 //
+#include <stdio.h>
+#include <iostream>
 
 #include <Neuron.h>
-#include <iostream>
 
 // Constructor
 Neuron::Neuron() {
-    bias = 0;   // Make random
+    bias = (double) std::rand() / (double) (RAND_MAX/100);
     value = 0;
-    
-    std::cout << "Neuron constructor!!" << std::endl;
 }
 
 Neuron::~Neuron() {
@@ -25,6 +24,8 @@ Connection::Connection(Neuron* pNeuron, double aWeight) {
 
 }
 
-void Neuron::addConnection(Neuron* pNeuronToConnect, double weight) {
+void Neuron::addConnection(Neuron* pNeuronToConnect) {
+    double weight = (double) std::rand() / (double) (RAND_MAX/100);
+    std::cout << "Connection made between: " << value << " and " << pNeuronToConnect->value << std::endl;
     input.push_back(new Connection(pNeuronToConnect, weight));
 }
